1ed313c23016e3d22683e20b7fdbf96e8e213828
fernandofernandezgonzalez <fernando.fernandez.gonzalez@gmail.com>
2021-02-05 13:44:03 +0100

    Update ejemploIris.R

--- ejemploIris.R
+++ ejemploIris.R
@@ -53,7 +53,7 @@ plot(puntuaciones$PC1,
 
 # Determinar número óptimo de clusters
 
-Ks<-2:15
+Ks<-1:15
 
 calculaWss<-function(k,data){
   
@@ -94,5 +94,35 @@ plot(puntuaciones$PC1,
      bg=c("red","green3","blue")[clustersFinales$cluster], 
      main="Edgar Anderson's Iris Data")
     
+##  Obtener el k programáticamente:
+# install.packages("inflection")
+require(inflection)
+# calcular codo, paquete inflection
+d2uik(1:length(wss),wss) # Recordar que el punto 2 es k=3!
 
+getElbowPoint <- function(x_values, y_values) {
+  
+  # Max values to create line
+  max_x_x <- max(x_values)
+  max_x_y <- y_values[which.max(x_values)]
+  max_y_y <- max(y_values)
+  max_y_x <- x_values[which.max(y_values)]
+  max_df <- data.frame(x = c(max_y_x, max_x_x), y = c(max_y_y, max_x_y))
+  
+  # Creating straight line between the max values
+  fit <- lm(max_df$y ~ max_df$x)
+  
+  # Distance from point to line
+  distances <- c()
+  for(i in 1:length(x_values)) {
+    distances <- c(distances, abs(coef(fit)[2]*x_values[i] - y_values[i] + coef(fit)[1]) / sqrt(coef(fit)[2]^2 + 1^2))
+  }
+  
+  # Max distance point
+  x_max_dist <- x_values[which.max(distances)]
+  y_max_dist <- y_values[which.max(distances)]
+  
+  return(c(x_max_dist, y_max_dist, max(distances)))
+} # Fuente: http://www.semspirit.com/artificial-intelligence/machine-learning/clustering/k-means-clustering/k-means-clustering-in-r/
+getElbowPoint(1:length(wss),wss) # También acierta con 3!
 
